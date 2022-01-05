3cd8fd3101a0c2da65cd6af40ac9fa89d06f1307
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-05 14:52:38 -0500

    linked .js to .html; successfully made fetch call to the charity API

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,19 @@
-<<<<<<< HEAD
+console.log("testnote");
 
+//Charity API ID and Key; The base URL for the Data API is:
+https://api.data.charitynavigator.org/v2
 
-
-
-//Below is where we will have the charity API code
 var app_id="97037ae1"
 var app_key = "3db6711569ba31d8872d4b3811e6e901";
+
+var charityQuery ="https://api.data.charitynavigator.org/v2/Organizations?app_id=97037ae1&app_key=3db6711569ba31d8872d4b3811e6e901&pageSize=50&search=food&rated=true&fundraisingOrgs=false&state=GA&city=Atlanta&scopeOfWork=REGIONAL"
+
+fetch(charityQuery);
+console.log(charityQuery);
+
+
+
+
 
 
 //Routing api key = 56317e1080cb40469433a05f077bbb52
@@ -21,4 +29,4 @@ var requestOptions = {
   
 
 console.log(requestOptions);
->>>>>>> 7625ea9b97fd13e1646507fdceeb931c9d355e7a
+//>>>>>>> 7625ea9b97fd13e1646507fdceeb931c9d355e7a
