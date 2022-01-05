0beb422f8a0b6a8fe88da1b5a44092625fea4c91
Emily Reese <emilyreese.ga@gmail.com>
2022-01-05 14:11:36 -0500

    I added the routing API to the JS file. We have the API key and object is fetched and logged to console.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
 something else is in this file
+// Routing api key = 56317e1080cb40469433a05f077bbb52
+var routingApiKey = "56317e1080cb40469433a05f077bbb52";
+
+var requestOptions = {
+    method: 'GET',
+  };
+  fetch("https://api.geoapify.com/v1/routing?waypoints=50.96209827745463%2C4.414458883409225%7C50.429137079078345%2C5.00088081232559&mode=drive&apiKey=56317e1080cb40469433a05f077bbb52", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  
+
+console.log(requestOptions);
