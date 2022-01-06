888d9ed686b9c9fef427091f7e1340c7098f047b
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-06 11:36:08 -0500

    made address into one variable

--- assets/script.js
+++ assets/script.js
@@ -11,11 +11,17 @@ var app_key = "3db6711569ba31d8872d4b3811e6e901";
 var charityQResult = document.getElementById("charitiesList");
 
 var charityQuery =
-	"https://api.data.charitynavigator.org/v2/Organizations?app_id=" + app_id +
-	"&app_key=" +	app_key +
-	"&search=" + charityQsearch +
-	"&fundraisingOrgs=false&state=" +	charityQState +
-	"&city=" +	charityQCity + "&scopeOfWork=ALL";
+	"https://api.data.charitynavigator.org/v2/Organizations?app_id=" +
+	app_id +
+	"&app_key=" +
+	app_key +
+	"&search=" +
+	charityQsearch +
+	"&fundraisingOrgs=false&state=" +
+	charityQState +
+	"&city=" +
+	charityQCity +
+	"&scopeOfWork=ALL";
 
 //a function that when called will run a query on charity API
 function fetchCharity() {
@@ -28,15 +34,28 @@ function fetchCharity() {
 			console.log(data);
 			for (var i = 0; i < data.length; i++) {
 				var charityQCard = document.createElement("p");
-				charityQCard.setAttribute("class","charityCard");
-				charityQCard.textContent = (data[i].charityName) + " Website: " + (data[i].websiteURL) + " Mailing Address: " + (data[i].mailingAddress.streetAddress1) + ", " + (data[i].mailingAddress.streetAddress2) + ", " + (data[i].mailingAddress.city) + ", " + (data[i].mailingAddress.stateOrProvince) + ", " + (data[i].mailingAddress.postalCode);
-        charityQResult.append(charityQCard);
+				var charityAddress = 	data[i].mailingAddress.streetAddress1 +
+				", " +
+				data[i].mailingAddress.streetAddress2 +
+				", " +
+				data[i].mailingAddress.city +
+				", " +
+				data[i].mailingAddress.stateOrProvince +
+				", " +
+				data[i].mailingAddress.postalCode;
+				charityQCard.setAttribute("class", "charityCard");
+				charityQCard.textContent =
+					data[i].charityName +
+					" Website: " +
+					data[i].websiteURL +
+					" Mailing Address: " +
+					charityAddress;
+				charityQResult.append(charityQCard);
 			}
 		});
 }
-console.log(charityQResult.value)
+console.log(charityQResult.value);
 //TODO: pull location details from charity api (as well as other elements we will display)
-
 
 //calling the function on click of a blue button
 // then running fetchCharity();
@@ -44,9 +63,6 @@ document.getElementById("blueButton").addEventListener("click", function () {
 	console.log("a button was clicked");
 	fetchCharity();
 });
-
-
-
 
 //Routing api key = 56317e1080cb40469433a05f077bbb52
 var routingApiKey = "56317e1080cb40469433a05f077bbb52";
@@ -66,13 +82,13 @@ console.log(requestOptions);
 //>>>>>>> 7625ea9b97fd13e1646507fdceeb931c9d355e7a
 
 //When the Charity is clicked on, store in local   (WORK IN PROGRESS)
-charityCard.addEventListener("click", function() {
+charityCard.addEventListener("click", function () {
 	var charityInfo = {
 		name: name.value,
 		mission: mission.value,
 		url: url.value,
-		location: location.value
-	}
+		location: location.value,
+	};
 	localStorage.setItem("viewed", JSON.stringify(charityInfo));
 
 	displayViewed();
