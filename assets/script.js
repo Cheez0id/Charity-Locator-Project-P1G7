233bdfcb553a6db3937f3c5437fa47306b70e522
Benjamin Kim <bennykim523@gmail.com>
2022-01-07 17:34:34 -0500

    working functions

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var app_id = "97037ae1";
 var app_key = "3db6711569ba31d8872d4b3811e6e901";
 var geocoder;
-var map; 
+var map;
 
 
 function initMap() {
@@ -10,11 +10,11 @@ function initMap() {
 	mapDiv = document.getElementById('map');
 	console.log(mapDiv);
 	map = new google.maps.Map(document.getElementById("map"),
-	{
-  center: {lat: 33.7490, lng: -84.3880 },
-  zoom: 8
-  });
-  }
+		{
+			center: { lat: 33.7490, lng: -84.3880 },
+			zoom: 8
+		});
+}
 
 
 var charityQResult = document.getElementById("charitiesList");
@@ -29,6 +29,7 @@ var charityQCard;
 
 //a function that when called will run a query on charity API
 function fetchCharity() {
+	console.log("click");
 	var charityQState = document.getElementById("State").value;
 	var charityQCity = document.getElementById("City").value;
 	var charityQZip = document.getElementById("zipCode").value;
@@ -63,68 +64,94 @@ function fetchCharity() {
 		.then(function (data) {
 			console.log(data);
 
-			function codeAddress (data){
-				for (i = 0; i <= data.length; i++){
+			// codeAddress(data);
+						displayEverything(data);
 
-				
-				var address = data[i].mailingAddress.streetAddress1
-				console.log(address);
-				geocoder.geocode({
-					"address": address
-				}, function (results, status){
-					if (status == 'OK') {
-							console.log(results);
-							var lat = results[0].geometry.location.lat();
-							console.log(lat);
-							var lng = results[0].geometry.location.lng();
-							console.log(lng);
-							var location = {lat: lat, lng: lng }
-						// map.setCenter(results[0].geometry.location);
-						var marker = new google.maps.Marker({
-							map: map,
-							position: location
-						});
-					  } else {
-						alert('Geocode was not successful for the following reason: ' + status);
-					  }
-				}
-				)
-			}
-				
-			}
-			codeAddress(data);
-
-
-			for (var i = 0; i <= data.length; i++) {
-
-				var charityQCard = document.createElement("p");
-				charityQCard.setAttribute("class", "charityCard");
-				charityTitle = data[i].charityName;
-				charityQCard.setAttribute("charityName", charityTitle);
-				charityURL = data[i].websiteURL;
-				charityQCard.setAttribute("charityUrl", charityURL);
-				charityAddress =
-					data[i].mailingAddress.streetAddress1 +
-					", " +
-					data[i].mailingAddress.streetAddress2 +
-					", " +
-					data[i].mailingAddress.city +
-					", " +
-					data[i].mailingAddress.stateOrProvince +
-					", " +
-					data[i].mailingAddress.postalCode;
-				charityQCard.setAttribute("charityAddress", charityAddress);
-
-				charityQCard.textContent =
-					charityTitle +
-					" Website: " +
-					charityURL +
-					" Mailing Address: " +
-					charityAddress;
-				charityQResult.append(charityQCard);
-			}
 		});
 }
+
+function displayEverything(data) {
+	console.log("all the cards");
+	for (var i = 0; i <= data.length; i++) {
+
+		var charityQCard = document.createElement("p");
+		charityQCard.setAttribute("class", "charityCard");
+		charityTitle = data[i].charityName;
+		charityQCard.setAttribute("charityName", charityTitle);
+		charityURL = data[i].websiteURL;
+		charityQCard.setAttribute("charityUrl", charityURL);
+		charityAddress =
+			data[i].mailingAddress.streetAddress1 +
+			", " +
+			data[i].mailingAddress.streetAddress2 +
+			", " +
+			data[i].mailingAddress.city +
+			", " +
+			data[i].mailingAddress.stateOrProvince +
+			", " +
+			data[i].mailingAddress.postalCode;
+		charityQCard.setAttribute("charityAddress", charityAddress);
+
+		charityQCard.textContent =
+			charityTitle +
+			" Website: " +
+			charityURL +
+			" Mailing Address: " +
+			charityAddress;
+		charityQResult.append(charityQCard);
+
+		var address = data[i].mailingAddress.streetAddress1
+		console.log(address);
+		geocoder.geocode({
+			"address": address
+		}, function (results, status) {
+			if (status == 'OK') {
+				console.log(results);
+				var lat = results[0].geometry.location.lat();
+				console.log(lat);
+				var lng = results[0].geometry.location.lng();
+				console.log(lng);
+				var location = { lat: lat, lng: lng }
+				// map.setCenter(results[0].geometry.location);
+				var marker = new google.maps.Marker({
+					map: map,
+					position: location
+				});
+			} else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			}
+		})
+
+	}
+};
+
+// function codeAddress(data) {
+// 	console.log("mappy");
+// 	for (var k = 0; k <= data.length; k++) {
+// 		var address = data[k].mailingAddress.streetAddress1
+// 		console.log(address);
+// 		geocoder.geocode({
+// 			"address": address
+// 		}, function (results, status) {
+// 			if (status == 'OK') {
+// 				console.log(results);
+// 				var lat = results[0].geometry.location.lat();
+// 				console.log(lat);
+// 				var lng = results[0].geometry.location.lng();
+// 				console.log(lng);
+// 				var location = { lat: lat, lng: lng }
+// 				 map.setCenter(results[0].geometry.location);
+// 				var marker = new google.maps.Marker({
+// 					map: map,
+// 					position: location
+// 				});
+// 			} else {
+// 				alert('Geocode was not successful for the following reason: ' + status);
+// 			}
+// 		})
+// 	}
+
+// }
 
 
 //calling the function on click of a blue button
@@ -133,13 +160,13 @@ document.getElementById("inputForm").addEventListener("submit", function (event)
 	event.preventDefault();
 	console.log("a button was clicked");
 	fetchCharity();
-	});
+});
 
-	//CLEAR BUTTON TO REFRESH PAGE
-	document.getElementById("clearBtn").addEventListener("click", function () {
-		console.log("a button was clicked");
-		window.location.reload();
-		});
+//CLEAR BUTTON TO REFRESH PAGE
+document.getElementById("clearBtn").addEventListener("click", function () {
+	console.log("a button was clicked");
+	window.location.reload();
+});
 
 
 //When the Charity is clicked on, store in local   (WORK IN PROGRESS)
@@ -187,4 +214,4 @@ function displayViewed() {
 
 
 
- 
+
